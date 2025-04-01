c3e309a22bb23f4ad3dc28f32f17730503cd1918
nicAmSarah <nicaciosarah085@gmail.com>
2025-03-31 23:55:04 -0300

    Normalize line endings

---
+++ src/app/components/title.tsx
@@ -0,0 +1,10 @@
+export default function Title(){
+    return(
+        <div>
+            <h1 className="w-full flex justify-center font-extrabold text-4xl tracking-wide text-center pt-20" >
+                WELCOME TO <br/>
+                SOLAR SYSTEM
+            </h1>
+        </div>
+    )
+}
